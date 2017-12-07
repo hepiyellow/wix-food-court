2a88dae8b89199f2f20ac84aac1daefa48b70d29
Erez Makavy <hepi.yellow@gmail.com>
2017-12-07 11:58:03 +0200

    Fix SearchResults test with DataTable

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DataTable from 'wix-style-react/DataTable';
 
+export const DATA_TABLE_DATA_HOOK = 'myDataTable';
 const SearchResults = props => {
   return (
     <DataTable
-      dataHook="dataTable"
+      dataHook={DATA_TABLE_DATA_HOOK}
       data={props.data}
       columns={[
         {title: 'Row Number', render: (row, rowNum) => '#' + (rowNum + 1), width: '20%', minWidth: '75px', important: true},
