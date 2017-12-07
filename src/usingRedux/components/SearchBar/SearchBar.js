ec69a71df6dd20e2ed4c84f6c6439f7174c12e88
Erez Makavy <hepi.yellow@gmail.com>
2017-12-07 21:53:04 +0200

    Convert SearchBar component to class

--- src/usingRedux/components/SearchBar/SearchBar.js
+++ src/usingRedux/components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import autobind from 'autobind-decorator';
 
 import s from './SearchBar.scss';
 import Search from 'wix-style-react/Search';
@@ -12,63 +13,69 @@ export const DATA_HOOKS = {
   staticDataCheckbox: 'staticDataCheckbox'
 };
 
-const SearchBar = props => {
-  log.debug('render(): props=', props);
-  // ESLint forces handler to be prefixed by 'handle'
-  const handleManuallyInputFilter = inputValue => {
-    props.updateFilter(inputValue);
-  };
 
-  // ESLint forces handler to be prefixed by 'handle'
-  const handleManuallyInputSearch = inputValue => {
-    props.fireSearch(inputValue);
-  };
+class SearchBar extends React.Component {
 
-  const handleCheckboxChange = _ref2 => {
+  @autobind
+  handleManuallyInputFilter(inputValue) {
+    this.props.updateFilter(inputValue);
+  }
+
+  @autobind
+  handleManuallyInputSearch(inputValue) {
+    this.props.fireSearch(inputValue);
+  }
+
+  @autobind
+  handleCheckboxChange(_ref2) {
     const isChecked = !!_ref2.target.checked;
     log.debug('handleCheckboxChange(): with isChecked=', isChecked);
-    props.setShowStaticData(isChecked);
-  };
+    this.props.setShowStaticData(isChecked);
+  }
 
-  const options = props.data.map((item, index) => {
-    return {
-      id: index,
-      value: item.title
-    };
-  });
+  render() {
+    log.debug('render(): props=', this.props);
 
-  //TODO: put inputs beside one another
-  return (
-    <div >
-      <div className={s.searchInput}>
-        <Search
-          id="search"
-          closeOnSelect={false}
-          onManuallyInput={handleManuallyInputSearch}
-          options={options}
-          placeholder="Search Term"
-          />
-        <Checkbox
-          dataHook={DATA_HOOKS.staticDataCheckbox}
-          checked={props.showStaticData}
-          onChange={handleCheckboxChange}
-          >
-          Show Static Data
-        </Checkbox>
+    const options = this.props.data.map((item, index) => {
+      return {
+        id: index,
+        value: item.title
+      };
+    });
+
+    //TODO: put inputs beside one another
+    return (
+      <div >
+        <div className={s.searchInput}>
+          <Search
+            id="search"
+            closeOnSelect={false}
+            onManuallyInput={this.handleManuallyInputSearch}
+            options={options}
+            placeholder="Search Term"
+            />
+          <Checkbox
+            dataHook={DATA_HOOKS.staticDataCheckbox}
+            checked={this.props.showStaticData}
+            onChange={this.handleCheckboxChange}
+            >
+            Show Static Data
+          </Checkbox>
+        </div>
+        <div className={s.filterInput}>
+          <Search
+            id="filter"
+            closeOnSelect={false}
+            onManuallyInput={this.handleManuallyInputFilter}
+            options={options}
+            placeholder="Filter By Name"
+            />
+        </div>
       </div>
-      <div className={s.filterInput}>
-        <Search
-          id="filter"
-          closeOnSelect={false}
-          onManuallyInput={handleManuallyInputFilter}
-          options={options}
-          placeholder="Filter By Name"
-          />
-      </div>
-    </div>
-  );
+    );
+  }
 
-};
+}
 
 SearchBar.propTypes = {
   data: PropTypes.array.isRequired,
