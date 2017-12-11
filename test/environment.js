7af062267ea2fe70171bed273b0af3185565e646
Erez Makavy <hepi.yellow@gmail.com>
2017-12-11 11:17:39 +0200

    Fix test environment to close server after each test

--- test/environment.js
+++ test/environment.js
@@ -1,6 +1,8 @@
 import server from '../src/server';
 
 const app = server();
+let httpServer;
 export const beforeAndAfter = (port = 3100) => {
-  before(() => new Promise(resolve => app.listen(port, resolve)));
+  before(() => new Promise(resolve => httpServer = app.listen(port, resolve)));
+  after(() => httpServer.close);
 };
